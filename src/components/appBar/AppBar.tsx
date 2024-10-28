acd4f5d6f6a57bc7020e03143e3c8a57ba7b05fe
ninexmobility <wworland@gmail.com>
2024-10-27 19:22:32 -0700

    remove secrets

--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -1,54 +1,89 @@
-// src/components/AppBar.tsx
+/**
+ * src/components/appBar/AppBar.tsx
+ * author: ninex
+ */
 
 import React, { useState } from "react";
 import "./AppBar.css";
 import { FaHome } from "react-icons/fa";
 import { GrProjects } from "react-icons/gr";
 import { PiReadCvLogoFill } from "react-icons/pi";
-import ResumeModal from "../resumeModal/ResumeModal"; // Ensure this path is correct
-
+import { useNavigate } from "react-router-dom";
+import ResumeModal from "../resumeModal/ResumeModal";
 import ProjectsModal from "../projectsModal/ProjectsModal";
 import { projects } from "../../data/projects";
 
+/**
+ * AppBar component renders the bottom navigation bar with icons for navigation.
+ * It includes buttons for Home, Projects, and Resume, and handles the opening
+ * and closing of the Resume and Projects modals.
+ *
+ * @component
+ * @returns {React.ReactElement} The AppBar component.
+ */
 const AppBar: React.FC = () => {
+    // State to control the visibility of the Resume modal
     const [showResume, setShowResume] = useState(false);
+    // State to control the visibility of the Projects modal
     const [showProjects, setShowProjects] = useState(false);
+    // Hook for navigation
+    const navigate = useNavigate();
 
+    /**
+     * Opens the Resume modal by setting showResume to true.
+     */
     const handleResumeOpen = () => {
         setShowResume(true);
     };
 
+    /**
+     * Closes the Resume modal by setting showResume to false.
+     */
     const handleResumeClose = () => {
         setShowResume(false);
     };
 
+    /**
+     * Opens the Projects modal by setting showProjects to true.
+     */
     const handleProjectsOpen = () => {
         setShowProjects(true);
     };
 
+    /**
+     * Closes the Projects modal by setting showProjects to false.
+     */
     const handleProjectsClose = () => {
         setShowProjects(false);
     };
+
     return (
         <div className='app-bar'>
-            <button aria-label='Home'>
+            {/* Home button navigates to the root path */}
+            <button
+                className='home-button'
+                onClick={() => navigate("/")}
+                aria-label='Home'>
                 <FaHome size={32} />
             </button>
+            {/* Projects button opens the Projects modal */}
             <button
-                className='resume-button'
+                className='projects-button'
                 onClick={handleProjectsOpen}
                 aria-label='Projects'>
-                <GrProjects size={32} />
+                <GrProjects size={24} />
             </button>
+            {/* Resume button opens the Resume modal */}
             <button
                 className='resume-button'
                 onClick={handleResumeOpen}
                 aria-label='Resume'>
-                <PiReadCvLogoFill size={32} />
+                <PiReadCvLogoFill size={24} />
             </button>
 
             {/* Resume Modal */}
             <ResumeModal show={showResume} handleClose={handleResumeClose} />
+
             {/* Projects Modal */}
             <ProjectsModal
                 show={showProjects}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
 );
